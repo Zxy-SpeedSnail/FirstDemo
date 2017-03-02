ac360f7e0a069278c1b31b10ac35fddeca3b29e1
zhouxueyuan <13622185659@163.com>
2017-03-02 15:59:58 +0800

    Thread   等等

---
+++ src/play/time.java
@@ -0,0 +1,11 @@
+package play;
+
+/**
+ * Created by yuan4j on 2017/2/22.
+ */
+public class time {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(args.length);
+    }
+}
